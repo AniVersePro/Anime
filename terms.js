5fb16e41f93f6b2575db6c1c2f07c6b0f913da56
AniVersePro <YK0146@outlook.com>
2025-03-13 00:28:47 +0530

    Update terms.js

--- terms.js
+++ terms.js
@@ -2,6 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const backBtn = document.getElementById('backBtn');
   backBtn.addEventListener('click', () => {
     // Redirect back to main page (adjust filename/path as needed)
-    window.location.href = 'we.html';
+    window.location.href = 'index.html';
   });
 });
